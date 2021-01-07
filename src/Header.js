b1d35929341a90130ae128c9637de33bb07240a0
Joe Turner <joe@oampo.co.uk>
2021-01-07 07:45:43 +0000

    Add logo

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCogs, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import logo from './images/logo.svg';
 import "./Header.css";
 
 export default function Header(props) {
   const { toggleSettings } = props;
   return (
     <header>
+      <img className="logo" src={logo} alt="Yolk logo"  />
       <h1 className="main-heading">Yolk</h1>
       <nav className="main-nav">
         <ul className="main-nav-list">
